ccbb4cecbd67cba775e02176e28f71f8435b4950
Lena <aoneko@gmx.de>
2018-09-29 22:51:03 +0200

    Initial commit

---
+++ src/main/java/de/berlin/lostberlin/LostberlinApplication.java
@@ -0,0 +1,12 @@
+package de.berlin.lostberlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostberlinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostberlinApplication.class, args);
+	}
+}

1bdae410d3f0d4872a656575cd78e9519f680bef
lenagainulina <aoneko@gmx.de>
2018-11-13 11:48:03 +0100

    - added @NotNull annotation to the parameters - adjusted imports to the new project folder structure - made method names more readable

--- src/main/java/de/berlin/lostberlin/repository/BusinessRepository.java
+++ src/main/java/de/berlin/lostberlin/repository/BusinessRepository.java
@@ -1,18 +1,21 @@
 package de.berlin.lostberlin.repository;
 
-import de.berlin.lostberlin.model.Business.Business;
-import de.berlin.lostberlin.model.Business.BusinessShortDao;
+import de.berlin.lostberlin.model.business.persistence.Business;
+import de.berlin.lostberlin.model.business.client.BusinessShortDao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Repository
 public interface BusinessRepository extends JpaRepository<Business, Long> {
 
-    @Query("SELECT new de.berlin.lostberlin.model.Business.BusinessShortDao (b.fName, b.lName, b.description, b.serviceLocation, b.photo)"+
+    @Query("SELECT new de.berlin.lostberlin.model.business.client.BusinessShortDao (b.fName, b.lName, b.description, b.serviceLocation, b.photo)"+
     "from Business b where b.serviceLocation = :serviceLocation")
-    List<BusinessShortDao> findAllByServiceLocation(@Param("serviceLocation") String serviceLocation);
+    List<BusinessShortDao> getShortBusinessProfiles(@NotNull @Param("serviceLocation") String serviceLocation);
+
+    boolean existsByEMail(String eMail);
+
 }
--- src/main/java/de/berlin/lostberlin/repository/OrderRepository.java
+++ src/main/java/de/berlin/lostberlin/repository/OrderRepository.java
@@ -1,24 +1,25 @@
 
 package de.berlin.lostberlin.repository;
 
-import de.berlin.lostberlin.model.Order.Order;
-import de.berlin.lostberlin.model.Order.OrderFullDao;
-import de.berlin.lostberlin.model.Order.OrderStatusDao;
+import de.berlin.lostberlin.model.order.persistence.Order;
+import de.berlin.lostberlin.model.order.client.OrderFullDao;
+import de.berlin.lostberlin.model.order.client.OrderStatusDao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import javax.validation.constraints.NotNull;
 
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, String> {
 
-    @Query("SELECT new de.berlin.lostberlin.model.Order.OrderStatusDao(o.name, o.date, o.time, o.businessId, o.status)"+
+    @Query("SELECT new de.berlin.lostberlin.model.order.client.OrderStatusDao (o.name, o.date, o.time, o.businessId, o.status)"+
     "from Order o where o.orderNr = :orderNr")
-    OrderStatusDao findByOrderNr(@Param("orderNr") String orderNr);
+    OrderStatusDao getOrderStatus(@NotNull @Param("orderNr") String orderNr);
 
-    @Query("SELECT new de.berlin.lostberlin.model.Order.OrderFullDao(o.name, o.phone, o.eMail, o.date, o.time, o.participantsNr, o.description, o.businessId, o.status, o.orderNr)"+
+    @Query("SELECT new de.berlin.lostberlin.model.order.client.OrderFullDao (o.name, o.phone, o.eMail, o.date, o.time, o.participantsNr, o.description, o.businessId, o.status, o.orderNr)"+
     "from Order o where o.orderNr = :orderNr AND o.status = :status AND o.businessId = :businessId")
-    OrderFullDao findByOrderNrAndStatusAndBusinessId(@Param("orderNr") String orderNr, @Param("status") Enum status, @Param("businessId") Long businessId);
+    OrderFullDao getFullOrder(@NotNull @Param("orderNr") String orderNr, @NotNull @Param("status") Enum status, @NotNull @Param("businessId") Long businessId);
 }
 
